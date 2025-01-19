c16d5d31b0a7f4352933b57b3a804c50a14657bf
tanisha0016 <155753436+tanisha0016@users.noreply.github.com>
2025-01-19 16:15:12 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,29 @@
-
+from distutils.core import setup
+setup(
+  name = 'Topsis-Tanisha-102203340',         # How you named your package folder (MyLib)
+  packages = ['Topsis-Tanisha-102203340'],   # Chose the same as "name"
+  version = '0.1',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'A Python package to perform TOPSIS analysis',   # Give a short description about your library
+  author = 'Tanisha Ahuja',                   # Type in your name
+  author_email = 'tanishaahuja666@gmail.com',      # Type in your E-Mail
+  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  long_description=open("README.md").read(),
+  long_description_content_type="text/markdown",
+  install_requires=[            # I get to this in a second
+          'pandas',
+          'numpy',
+      ],
+  entry_points={
+        'console_scripts': [
+            'topsis=Topsis.topsis:main',  # Entry point for CLI usage
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
